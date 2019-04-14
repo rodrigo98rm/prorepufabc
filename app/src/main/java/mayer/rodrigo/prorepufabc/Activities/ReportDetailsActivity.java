e77cdb528cda40a35eea2de8f2bf2ec692d0aa83
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2019-04-14 10:33:58 -0300

    NewReport Activity layout

---
+++ app/src/main/java/mayer/rodrigo/prorepufabc/Activities/ReportDetailsActivity.java
@@ -0,0 +1,16 @@
+package mayer.rodrigo.prorepufabc.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import mayer.rodrigo.prorepufabc.R;
+
+import android.os.Bundle;
+
+public class ReportDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_details);
+    }
+
+}
