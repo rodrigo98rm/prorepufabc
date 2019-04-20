ef861d5288ab6cbfe1d8e5e49a8dcb7023d7991a
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2019-04-20 13:23:35 -0300

    My Reports Activity layout

--- app/src/main/java/mayer/rodrigo/prorepufabc/Activities/MyReportsActivity.java
+++ app/src/main/java/mayer/rodrigo/prorepufabc/Activities/MyReportsActivity.java
@@ -1,6 +1,8 @@
 package mayer.rodrigo.prorepufabc.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import mayer.rodrigo.prorepufabc.Fragments.ReportsListFragment;
 import mayer.rodrigo.prorepufabc.R;
 
 import android.os.Bundle;
@@ -14,5 +16,22 @@ public class MyReportsActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Meus relatos");
 
+        insertFragment();
+
+
     }
+
+    private void insertFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        ReportsListFragment fragment = new ReportsListFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ReportsListFragment.FRAGMENT_TYPE, ReportsListFragment.USER);
+        fragment.setArguments(bundle);
+
+        ft.replace(R.id.frameLayout_MyReports, fragment);
+
+        ft.commit();
+    }
+
 }
--- app/src/main/java/mayer/rodrigo/prorepufabc/Fragments/ReportsListFragment.java
+++ app/src/main/java/mayer/rodrigo/prorepufabc/Fragments/ReportsListFragment.java
@@ -70,6 +70,19 @@ public class ReportsListFragment extends Fragment {
             photosUrls.add("https://firebasestorage.googleapis.com/v0/b/prorepufabc.appspot.com/o/images%2FIz5K1w1F8AQPrwZpJBPCWwMOKQg1.jpg?alt=media&token=4f27659d-acb0-42ca-8943-29df12551307");
             Report report = new Report(user, "Saída de emergência danificada", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 1345, 1555268610000L, photosUrls);
             reports.add(report);
+        }else if(type == USER){
+            User user = new User("Rodrigo Rominho Mayer", "https://firebasestorage.googleapis.com/v0/b/prorepufabc.appspot.com/o/images%2FIz5K1w1F8AQPrwZpJBPCWwMOKQg1.jpg?alt=media&token=4f27659d-acb0-42ca-8943-29df12551307");
+            ArrayList<String> photosUrls = new ArrayList<>();
+            photosUrls.add("https://images.unsplash.com/photo-1555570181-c590c39f61fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80");
+            photosUrls.add("https://images.unsplash.com/photo-1555579877-8887dfaed207?ixlib=rb-1.2.1&auto=format&fit=crop&w=1601&q=80");
+            Report report = new Report(user, "Goteira na rampa", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 1345, 1555268610000L, new ArrayList<>(photosUrls));
+            reports.add(report);
+
+            photosUrls.clear();
+            photosUrls.add("https://images.unsplash.com/photo-1555284023-249222086985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+            photosUrls.add("https://images.unsplash.com/photo-1555086196-a28d9bc9d677?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+            report = new Report(user, "Buraco no teto", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 1345, 1555268610000L, new ArrayList<>(photosUrls));
+            reports.add(report);
         }
 
         ReportListAdapter adapter = new ReportListAdapter(reports);
